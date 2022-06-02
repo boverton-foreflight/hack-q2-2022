a32a523e8b67b1cda8124e0b152407b15b8955fe
Brandon Overton <boverton@foreflight.com>
2022-06-02 14:05:36 -0500

    Yugest Change ever

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the biggest change I've ever seen.
         </p>
         <a
           className="App-link"
